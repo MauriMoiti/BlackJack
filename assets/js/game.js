853b19e6f3c5d6426f0503d718dc228e2b99922b
Mauricio Moitiño <maurimoitii@gmail.com>
2024-02-06 14:08:56 -0300

    the click event was created to get a card, also in the last event was created a system of sum of points for the players

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,6 +9,26 @@ const specialCards = ['A','J','Q','K'];
 
 let deck = [];
 
+let playerPoints = 0;
+let cpuPoints = 0;
+
+// references HTML 
+const buttonNewGame = document.querySelector('#buttonNewGame');
+const buttonGetCard = document.querySelector('#buttonGetCard');
+const buttonStopCards = document.querySelector('#buttonStopCards');
+
+const showPoints = document.getElementsByTagName('small');
+
+
+// Events
+buttonGetCard.addEventListener('click', () => {
+    let cardObtained = getCard();
+    
+    playerPoints += getValueCard(cardObtained);
+    
+    showPoints[0].innerText = playerPoints;
+});
+
 // create new deck for the players
 const createDeck = () => {
     for(let i = 2; i <= 10; i++) {
@@ -23,12 +43,12 @@ const createDeck = () => {
     } // add special cards to the deck
 
     deck = _.shuffle(deck); // underscore library --> Shuffle the deck randomly
-    console.log(deck);
 }
 createDeck();
 
 // get a card
 let deckCopy = Array.from(deck); // In this case, i prefer mantain deck integrity throughout the game. 
+
 
 const getCard = () => (deckCopy.length !== 0) ? deckCopy.shift() : console.error("There aren't more cards in the deck");
 /* alternative code: 
@@ -41,7 +61,6 @@ const getCard = () => (deckCopy.length !== 0) ? deckCopy.shift() : console.error
 */
 
 const getValueCard = (card) => {
-    let points = 0;
     let cardValue = card.substring(0, card.length - 1); // getValueCard(2k) --> output: 2,  getValueCard(10c) --> outout: 10, getValueCard(AC) --> output: A
     return isNaN(cardValue) ? (cardValue === 'A' ? 11 : 10) : Number(cardValue);
 
@@ -50,6 +69,6 @@ const getValueCard = (card) => {
     /* 
         console.log(getValueCard('AD')) // 11
         console.log(getValueCard('2D')) // 2
-        console.log(getValueCard('KH')) // 10
+        console.log(getValueCard('KH')) // 10 
     */
 }
